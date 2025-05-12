58638a6a74815d23c680efa299bc150849dcd25a
nedairisz <iriszneda@gmail.com>
2025-05-12 12:58:09 +0200

    tev

--- app/Http/Controllers/TevekenysegController.php
+++ app/Http/Controllers/TevekenysegController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tevekenyseg;
 use Illuminate\Http\Request;
 
 class TevekenysegController extends Controller
 {
-    //
+    public function index(){
+        return Tevekenyseg::all();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\BejegyzesekController;
+use App\Http\Controllers\TevekenysegController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/tevekenysegek', [TevekenysegController::class, 'index']);
 Route::get('/bejegyzesek', [BejegyzesekController::class, 'index']);
 Route::get('/bejegyzesek/{osztaly_nev}', [BejegyzesekController::class, 'osztalySzerint']);
 Route::post('/bejegyzes', [BejegyzesekController::class, 'ujBejegyzes']);
